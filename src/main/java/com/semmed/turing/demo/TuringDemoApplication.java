d677303314fa953262e270308be55b645023cb0a
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2025-01-14 18:41:17 +0400

    first initialization

---
+++ src/main/java/com/semmed/turing/demo/TuringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.semmed.turing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semmed/turing/demo/TuringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semmed.turing.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
